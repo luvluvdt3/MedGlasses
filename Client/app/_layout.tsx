adf297dcaae9451c639dfd57bd5464ad07a8071c
Thi Thanh Tu DUONG <77581509+luvluvdt3@users.noreply.github.com>
2025-01-02 08:07:45 +0100

    #3 fix headerShown

--- Client/app/_layout.tsx
+++ Client/app/_layout.tsx
@@ -4,25 +4,23 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler'
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack>
+      <Stack
+        screenOptions={{
+          headerShown: false
+        }}>
         <Stack.Screen 
           name="index" 
-          options={{ 
-            headerShown: false 
-          }} 
         />
         <Stack.Screen 
           name="incoming-call" 
           options={{
             presentation: 'modal',
-            headerShown: false
           }}
         />
         <Stack.Screen 
           name="ongoing-call" 
           options={{
             presentation: 'modal',
-            headerShown: false,
           }}
         />
       </Stack>
